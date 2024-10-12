1cef2f6e27091d9f226cf4fc5ca254ea6c7ff606
Nikita Filin <nikita10filin@gmail.com>
2024-10-12 01:54:03 -0700

    small fix

--- cmdlets/executeADCommand.ps1
+++ cmdlets/executeADCommand.ps1
@@ -13,7 +13,6 @@ function Execute-ADCommand {
     }
 }
 
-
 $command = "Get-ADUser -Filter * -Property Name | Select-Object -First 50 Name"
 
 #$command = 'New-ADUser -Name "Alicee Johnson" -GivenName "Alicee" -Surname "Johnsonn" -SamAccountName "ajohnson011" -UserPrincipalName "ajohnnson01@domain.com" -Path "CN=Users,DC=Domain,DC=ee" -AccountPassword (ConvertTo-SecureString "ComplexP@ssw0rd4567" -AsPlainText -Force) -Enabled $true'
