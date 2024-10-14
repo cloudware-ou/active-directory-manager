40fb4a756e47d9ffe84d965b7ed7dd3b85d8bc8d
NFilin10 <70175534+NFilin10@users.noreply.github.com>
2024-10-14 17:27:16 +0300

    Update ActiveDirectoryRestApiApplicationTests.java

--- rest-api/src/test/java/com/nortal/activedirectoryrestapi/ActiveDirectoryRestApiApplicationTests.java
+++ rest-api/src/test/java/com/nortal/activedirectoryrestapi/ActiveDirectoryRestApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ActiveDirectoryRestApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    //@Test
+    //void contextLoads() {
+    //}
 
 }
